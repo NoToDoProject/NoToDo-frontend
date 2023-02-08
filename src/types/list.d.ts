30f3ca1981be95868b98b26d00c690cf282489da
Okysu <yby@ecanse.com>
2023-02-09 00:58:51 +0800

    add: todolist type

---
+++ src/types/list.d.ts
@@ -0,0 +1,29 @@
+type step = {
+    key: string,
+    value: string,
+    isFinished?: boolean,
+}
+
+enum repeat {
+    once = 1,
+    weekday = 2,
+    weekend = 3,
+}
+
+type list = {
+    id: string,
+    key: string,
+    value: string,
+    steps?: step[],
+    repeat?: number | string | repeat,
+    isFinished?: boolean,
+    description?: string,
+    file?: string,
+    originMyDay?: boolean,
+    originImportant?: boolean,
+    originPlan?: boolean,
+    isMyDay?: boolean,
+    isImportant?: boolean,
+    isPlan?: boolean,
+    creatTime: number,
+}
